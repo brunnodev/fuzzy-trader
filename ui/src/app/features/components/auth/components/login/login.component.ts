f7ac039bbd0abb2ec8857a4cf146e855d91b1f16
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 09:58:09 -0300

    Created login component

---
+++ ui/src/app/features/components/auth/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { AuthState } from '../../store/reducers/auth.reducer';
+import { RequestLogin } from '../../store/actions/auth.actions';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authStore: Store<AuthState>
+  ) { }
+
+  login(formValue) {
+    if (this.loginForm.valid) {
+      this.authStore.dispatch(new RequestLogin({
+        email: formValue.email,
+        password: formValue.password
+      }))
+    }
+  }
+
+  navigateToSignup() {
+    this.router.navigate(['/auth/signup'])
+  }
+}
