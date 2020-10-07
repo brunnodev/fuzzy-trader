c9ca33d3c401e53a1c9aeb1a350550487e6c7176
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 12:33:43 -0300

    Improved assets dialog

--- ui/src/app/features/components/asset/components/assets-dialog/assets-dialog.component.ts
+++ ui/src/app/features/components/asset/components/assets-dialog/assets-dialog.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { select, Store } from '@ngrx/store';
+import { RequestToCreateInvestment } from 'app/features/components/investment/store/actions/investment.actions';
+import { InvestmentState } from 'app/features/components/investment/store/reducers/investment.reducer';
 
 import { AssetState } from '../../store/reducers/asset.reducer';
-import { selectAssetList } from '../../store/selectors/asset.selectors';
+import { selectSuggestedAssetList } from '../../store/selectors/asset.selectors';
 
 @Component({
   selector: 'app-assets-dialog',
@@ -13,13 +15,14 @@ import { selectAssetList } from '../../store/selectors/asset.selectors';
 })
 export class AssetsDialogComponent {
 
-  assets$ = this.assetStore.pipe(
-    select(selectAssetList)
+  suggestedAssets$ = this.assetStore.pipe(
+    select(selectSuggestedAssetList)
   )
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public valueToInvest,
     private assetStore: Store<AssetState>,
+    private investmentStore: Store<InvestmentState>,
     private dialogRef: MatDialogRef<AssetsDialogComponent>
   ) { }
 
@@ -31,5 +34,7 @@ export class AssetsDialogComponent {
     }
 
     this.dialogRef.close()
+
+    this.investmentStore.dispatch(new RequestToCreateInvestment({ investmentToCreate }))
   }
 }
