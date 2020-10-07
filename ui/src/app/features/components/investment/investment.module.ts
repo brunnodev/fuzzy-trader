ba1501c6bd1f3c899a181fb68ea2345ed304e605
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 11:53:35 -0300

    Applied store and component on the module

---
+++ ui/src/app/features/components/investment/investment.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { ListComponent } from './components/list/list.component';
+import { InvestmentRoutingModule } from './investment-routing.module';
+import { InvestmentService } from './services/investment.service';
+import { InvestmentEffects } from './store/effects/investment.effects';
+import * as fromInvestment from './store/reducers/investment.reducer';
+
+@NgModule({
+  declarations: [
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    InvestmentRoutingModule,
+
+    /* Store */
+    StoreModule.forFeature('investment', fromInvestment.reducer),
+    EffectsModule.forFeature([ InvestmentEffects ])
+  ],
+  providers: [
+    InvestmentService
+  ]
+})
+export class InvestmentModule { }
