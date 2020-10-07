1acfd1d5c2b85057e56bb0100be9ace7bbc49031
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 09:27:24 -0300

    Applied interceptors

--- ui/src/app/core/core.module.ts
+++ ui/src/app/core/core.module.ts
@@ -1,11 +1,40 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
+import { URLInterceptor } from './interceptors/url.interceptor';
+import { SessionModule } from './session/session.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SessionModule
   ],
-  providers: []
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: URLInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
