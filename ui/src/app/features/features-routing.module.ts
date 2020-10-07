51e897f70757d27813e832beb52011a319627faa
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 12:33:55 -0300

    Fixed features routing

--- ui/src/app/features/features-routing.module.ts
+++ ui/src/app/features/features-routing.module.ts
@@ -5,6 +5,20 @@ import { AdminLayoutComponent } from '../layouts/admin-layout/admin-layout.compo
 
 const routes: Routes = [
   {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'assets',
+        loadChildren: './components/asset/asset.module#AssetModule'
+      },
+      {
+        path: 'investments',
+        loadChildren: './components/investment/investment.module#InvestmentModule'
+      }
+    ]
+  },
+  {
     path: 'auth',
     loadChildren: './components/auth/auth.module#AuthModule'
   }
--- ui/src/app/features/features.module.ts
+++ ui/src/app/features/features.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AssetModule } from './components/asset/asset.module';
 import { AuthModule } from './components/auth/auth.module';
 import { FeaturesRoutingModule } from './features-routing.module';
 
@@ -9,7 +10,8 @@ import { FeaturesRoutingModule } from './features-routing.module';
   imports: [
     CommonModule,
     FeaturesRoutingModule,
-    AuthModule
+    AuthModule,
+    AssetModule
   ]
 })
 export class FeaturesModule { }
