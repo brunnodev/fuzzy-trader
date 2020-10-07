394f35c11ede4a136107e35b9b67cc070e2b90e0
Brunno Romero <brunnoromerotenorio@gmail.com>
2020-10-07 10:23:47 -0300

    Created asset asset Data Source List

---
+++ api/modules/asset/asset.config.js
@@ -0,0 +1,45 @@
+const { normalizeCoinapi } = require('./adapters/coinapi')
+const { normalizeYahooFinance } = require('./adapters/yahoo-finance')
+
+const DataSourceTypeEnum = {
+  'Stock': 'Stock',
+  'Crypto': 'Crypto'
+}
+
+const DataSourceMethodEnum = {
+  'GET': 'get',
+  'POST': 'post'
+}
+
+const assetDataSourceList = [
+  {
+    name: 'yahoo-finance',
+    request: {
+      url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=',
+      headers: {
+        'X-RapidAPI-Key': 'b5db1028ddmsh61ae57f0ae42b2fp148c09jsnbc0148faa5f5'
+      }
+    },
+    method: DataSourceMethodEnum.POST,
+    type: DataSourceTypeEnum.Stock,
+    assetIds: [ 'AMD', 'IBM', 'AAPL', 'FB', 'AMZN', 'TSLA' ],
+    assetIdsDivider: ',',
+    normalize: normalizeYahooFinance
+  },
+  {
+    name: 'coinapi',
+    request: {
+      url: 'https://rest.coinapi.io/v1/assets?filter_asset_id='
+    },
+    method: DataSourceMethodEnum.GET,
+    type: DataSourceTypeEnum.Crypto,
+    assetIds: [ 'LTC', 'BTC', 'XRP', 'NMC', 'BLC', 'BTS', 'BTX', 'ETH', 'EOS' ],
+    assetIdsDivider: ';',
+    normalize: normalizeCoinapi
+  }
+]
+
+module.exports = {
+  assetDataSourceList,
+  DataSourceTypeEnum
+}
